97d44bf4aaebb03000c994d116f4709b64742b42
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-02-26 19:17:27 +0300

    Added sum_numbers method.

--- pyro-app/server.py
+++ pyro-app/server.py
@@ -10,6 +10,12 @@ class GreetingMaker(object):
         num = random.randint(0, 10**6)
         return "Hello, " + name + " your lucky number is " + str(num)
 
+    @staticmethod
+    def sum_numbers(self, name, first, second):
+        print(name + " is asking me to make a summation")
+        num = int(first) + int(second)
+        return "Hello, " + name + " your result is " + str(num)
+
 
 daemon = Pyro4.Daemon()
 uri = daemon.register(GreetingMaker)
