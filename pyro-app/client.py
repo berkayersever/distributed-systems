6a87efc7a1e595c81863727402f41e3641f567b5
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-02-26 19:34:27 +0300

    Implemented the client side.

--- pyro-app/client.py
+++ pyro-app/client.py
@@ -2,3 +2,14 @@ import Pyro4
 
 uri = input("What is the URI of the greeting object?").strip()
 server_ref = Pyro4.Proxy(uri)
+
+greeting_maker = Pyro4.Proxy("PYRONAME:example.greeting")
+
+name = input("What is your name?").strip()
+# response = server_ref.get_fortune(name)
+
+first = input("Your first input? ").strip()
+second = input("Your second input? ").strip()
+response = greeting_maker.sum_numbers(name, first, second)
+
+print(response)
