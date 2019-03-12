c5c5094f5c0e34ceb7d337de0d1ed96376ebb937
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-12 23:51:24 +0300

    Implemented a new put method.

--- flight-rest-api/flight.py
+++ flight-rest-api/flight.py
@@ -6,6 +6,9 @@ tickets = []
 
 
 class Flight(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
+
     def get(self):
         print("Get Request")
         data = (request.get_json())
@@ -20,8 +23,18 @@ class Flight(Resource):
         else:
             return {'message': 'Ticket not found'}, 404
 
-    def put(self):
-        data = (request.get_json())
-        PNR = data['PNR']
-        tickets.append(PNR)
-        return {'PNR':data['PNR']}, 201
+    # def put(self):
+    #     data = (request.get_json())
+    #     PNR = data['PNR']
+    #     tickets.append(PNR)
+    #     return {'PNR':data['PNR']}, 201
+
+    def put(self, name):
+        data = Flight.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
