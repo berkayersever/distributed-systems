f60851939adc4ca6727bcf730dd613ecf0cad89a
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-12 18:58:03 +0300

    Added the nonce and initialized the app.

--- flight-rest-api/app.py
+++ flight-rest-api/app.py
@@ -5,6 +5,10 @@ from flask import Flask, jsonify, request
 from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
+app.secret_key = "k8!vv3&m=g_p0-6w6do_&q05_6%ade5cs8^5*@zk=#kv16kd@+"
 api = Api(app)
 
 tickets = []
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
