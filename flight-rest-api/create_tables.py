98ae98bae539d2bb0df4ef2905168a14db325f89
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-12 23:43:08 +0300

    Minor change in the database creation.

--- flight-rest-api/create_tables.py
+++ flight-rest-api/create_tables.py
@@ -9,7 +9,7 @@ cursor.execute(create_table)
 insert_admin = "INSERT INTO users (id, username, password) VALUES (1, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918')"
 cursor.execute(insert_admin)
 
-create_table = "CREATE TABLE IF NOT EXISTS flights (dest TEXT, from TEXT, date TEXT, flight_id INTEGER )"
+create_table = "CREATE TABLE IF NOT EXISTS flights (flight_id INTEGER PRIMARY KEY, to_where TEXT, from_where TEXT, date TEXT)"
 cursor.execute(create_table)
 
 connection.commit()
