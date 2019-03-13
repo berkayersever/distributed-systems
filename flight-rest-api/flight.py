100233d119e16e0cb6ad3fb579c6280f88a47577
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-13 20:03:05 +0300

    Added HTTP Status Code for /flights

--- flight-rest-api/flight.py
+++ flight-rest-api/flight.py
@@ -85,4 +85,4 @@ class FlightList(Resource):
         for row in result:
             flights.append({'to_where': row[1], 'from_where': row[2], 'date': row[3], 'flight_id': row[0]})
         connection.close()
-        return {'flights': flights}
+        return {'flights': flights}, 200
