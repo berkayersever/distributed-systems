4e16dc3420833a54d277e365af40e1931476685c
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-16 01:10:57 +0300

    Refactored the code.

--- flight-rest-api/flight.py
+++ flight-rest-api/flight.py
@@ -12,6 +12,10 @@ def generate_pnr(length=10, allowed_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     return ''.join(random.choice(allowed_chars) for i in range(length))
 
 
+def generate_id(flight_id):
+    return 'TK' + str(1900 + flight_id)
+
+
 class Flight(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('to_where', help="This field cannot be left blank!")
@@ -154,7 +158,6 @@ class Ticket(Resource):
     parser.add_argument('PNR')
     parser.add_argument('seat_number', type=int)
     parser.add_argument('flight_id', type=int, help="This field cannot be left blank!")
-    print(parser)
 
     @classmethod
     def insert(cls, ticket):
@@ -212,22 +215,11 @@ class Ticket(Resource):
     def find_by_pnr(cls, pnr):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
-        # print(pnr)
-        # _id = 'TK' + str(1900 + ticket['flight_id'])  # flight_id to be used in new table
-
         query = "SELECT * FROM tickets WHERE PNR=?"
-        # result = cursor.execute(query, (flight['to_where'], flight['from_where'], flight['date']))
         result = cursor.execute(query, (pnr,))
-
-
-        # query = "SELECT * FROM {} WHERE flight_id=?".format(_id)
-        # result = cursor.execute(query, (ticket['flight_id'],))
         row = result.fetchone()
-        # print(row)
         connection.close()
         if row:
-            # print(row)
-            # return {'to_where': row[2], 'from_where': row[3], 'date': row[4], 'flight_id': row[5]}
             return {'to_where': row[2], 'from_where': row[3], 'date': row[4], 'flight_id': row[5], 'seat_number': row[1]} # Seat number is not needed probably
             # return {'ticket': {'to_where': row[2], 'from_where': row[3], 'date': row[4], 'flight_id': row[5], 'seat_number': 1}} # Alternative Format
 
@@ -289,84 +281,42 @@ class Ticket(Resource):
         connection.close()
         return {'flights': flights}, 200
 
-
-
-
-    # def get(self):
-    #     data = Ticket.parser.parse_args()
-    #     ticket = self.find_by_pnr(data['PNR'])
-    #     if ticket:
-    #         return ticket, 200
-    #     return {'message': 'Flight not found'}, 404
-
-
-    # def get(self, flight_id):
-    #     ticket = self.find_by_id(flight_id)
-    #     if ticket:
-    #         return ticket, 200
-    #     return {'message': 'Flight not found'}, 404
-
     def post(self):
         data = Ticket.parser.parse_args()
-        print("DATA: ")
-        print(data)
-        # pnr = data['PNR']
         ticket = {'PNR': data['PNR']}
-        print(ticket)
-        # print('ticket: ' + str(ticket))
-        # print(pnr)
-        # print(data)
-        # ticket = {'PNR': data['PNR']}
-        # print(ticket)
-        # pnr = ticket['PNR']
-        # print(pnr)
-        # 'ticket': {'to_where': row[2], 'from_where': row[3], 'date': row[4], 'flight_id': row[5], 'seat_number': 1}}
         temp = self.find_by_pnr(ticket['PNR'])
-        print(temp)
-        # if self.find_by_pnr(ticket['PNR']):
         if temp:
             connection = sqlite3.connect('data.db')
             cursor = connection.cursor()
             query = "SELECT count(*) FROM tickets WHERE seat_number=? AND flight_id=?"
             result = cursor.execute(query, (data['seat_number'], temp['flight_id'],))
-            print(temp['seat_number'])
-            print(ticket['PNR'])
             row = result.fetchone()
-            connection.close()
             if row[0] == 0:
+                query = "UPDATE tickets SET seat_number =? WHERE PNR=?"
+                cursor.execute(query, (data['seat_number'], ticket['PNR'],))
+                connection.commit()
+                connection.close()
                 return {'PNR': ticket['PNR'], 'seat_number': data['seat_number']}, 200
+            else:
+                connection.close()
+                return {'message': "Seat number {} is not available for {}.".format(data['seat_number'], generate_id(temp['flight_id']))}, 409  # Seat Not Available
+        else:
+            return {'message': "PNR: {} does not exist".format(ticket['PNR'])}, 404    # PNR Not Found
 
-            # row = result.fetchone
-
-            # result = cursor.execute(query, (flight['to_where'], flight['from_where'], flight['date']))
-            # result = cursor.execute(query, (pnr,))
-
-
-        if not self.find_by_pnr(ticket['PNR']):
-            # return {"message": "PNR {} does not exist"}.format('s'), 404     # Not Found
-            return {'message': "PNR: {} does not exist".format(ticket['PNR'])}, 404    # Not Found
-
-
-        # if not self.find_by_pnr(ticket['PNR']):
-        #     print(ticket['PNR'])
-        #     return {'message': "A flight with id '{}' already exists.".format(flight_id)}, 400
-
-        data = Flight.parser.parse_args()
-        flight = {'flight_id': flight_id, 'to_where': data['to_where'],
-                  'from_where': data['from_where'], 'date': data['date']}
-        try:
-            self.insert(flight)
-        except RuntimeError:
-            return {"message": "An error occurred while inserting the flight."}, 500  # Internal Server Error
-        return flight, 201
-
-    def delete(self, flight_id):
+    def delete(self):
+        data = Ticket.parser.parse_args()
+        ticket = {'PNR': data['PNR']}
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
-        if not self.find_by_id(flight_id):
-            return {'message': "A flight with id '{}' does not exist.".format(flight_id)}, 404
-        query = "DELETE FROM flights WHERE flight_id=?"
-        cursor.execute(query, (flight_id,))
-        connection.commit()
-        connection.close()
-        return {'message': 'Flight deleted'}, 200
+        query = "SELECT * FROM tickets WHERE PNR=?"
+        result = cursor.execute(query, (ticket['PNR'],))
+        row = result.fetchone()
+        if row:
+            query = "DELETE FROM tickets WHERE PNR=?"
+            cursor.execute(query, (ticket['PNR'],))
+            connection.commit()
+            connection.close()
+            return {'message': "Ticket with PNR: {} has been cancelled".format(ticket['PNR'])}, 200  # Cancel Ticket
+        else:
+            connection.close()
+            return {'message': "PNR: {} does not exist".format(ticket['PNR'])}, 404  # PNR Not Found
