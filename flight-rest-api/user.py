d03467d640fe4dd0813bffdcbaa59e1d509ef9f2
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-16 02:18:25 +0300

    Implemented find_by_username method.

--- flight-rest-api/user.py
+++ flight-rest-api/user.py
@@ -6,3 +6,17 @@ class User:
         self.id = _id
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+        connection.close()
+        return user
