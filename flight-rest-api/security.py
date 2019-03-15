ed7d76fb6e6e8c92c022530d23d9c89173dc250c
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-16 02:32:21 +0300

    Implemented the identity method.

--- flight-rest-api/security.py
+++ flight-rest-api/security.py
@@ -12,3 +12,8 @@ def authenticate(username, password):
     user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, encrypt_string(password)):
         return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return User.find_by_id(user_id)
