426472ad89657457d313cb3aca8ddc9f0f3304dd
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-27 23:43:27 +0300

    Minor changes.

--- message-queueing-system/app.py
+++ message-queueing-system/app.py
@@ -17,11 +17,11 @@ class MapReduce(ABC):
         super().__init__()
 
     @abstractmethod
-    def map(self, data_arr):                            # data_arr: Integer Array
+    def map(self, data_arr):        # data_arr: Integer Array
         self.data_arr_map = data_arr
 
     @abstractmethod
-    def reduce(self, data_arr):                         # data_arr: Integer Array
+    def reduce(self, data_arr):     # data_arr: Integer Array
         self.data_arr_reduce = data_arr
 
     def __producer(self, data_arr):
@@ -31,20 +31,20 @@ class MapReduce(ABC):
         zmq_socket.bind("tcp://127.0.0.1:5557")
         while len(data_arr) < len(self.worker_process):
             data_arr.append(None)
-        chunk = math.floor(len(data_arr) / len(self.worker_process))
-        first = chunk + (len(data_arr) % len(self.worker_process))
+        block = math.floor(len(data_arr) / len(self.worker_process))
+        first = block + (len(data_arr) % len(self.worker_process))
         point = 0
         while first <= len(data_arr):
             message = {'array': data_arr[point: first]}
             zmq_socket.send_json(message)
-            point = point + first if point == 0 else point + chunk
-            first += chunk
+            point = point + first if point == 0 else point + block
+            first += block
 
     def __consumer(self):
         context = zmq.Context()
-        consumer_receiver = context.socket(zmq.PULL)    # Receive Work
+        consumer_receiver = context.socket(zmq.PULL)  # Receive Work
         consumer_receiver.connect("tcp://127.0.0.1:5557")
-        consumer_sender = context.socket(zmq.PUSH)      # Send Work
+        consumer_sender = context.socket(zmq.PUSH)  # Send Work
         consumer_sender.connect("tcp://127.0.0.1:5558")
         while True:
             work = consumer_receiver.recv_json()
@@ -63,10 +63,10 @@ class MapReduce(ABC):
             collector_data.append(result['consumer_result'])
         print("{} Result: {}".format(str(self.__class__.__name__), self.reduce(collector_data)))
 
-    def start(self, data_arr):                          # data_arr: Integer Array
-        self.process_result_collector = Process(target=self.__result_collector,)
+    def start(self, data_arr):  # data_arr: Integer Array
+        self.process_result_collector = Process(target=self.__result_collector, )
         for i in range(self.num_worker):
-            process_consumer = Process(target=self.__consumer,)
+            process_consumer = Process(target=self.__consumer, )
             process_consumer.start()
             self.worker_process.append(process_consumer)
         self.process_result_collector.start()
@@ -118,7 +118,7 @@ if __name__ == '__main__':
         [res.append(random.randint(start, end)) for _ in range(num)]
         return res
 
-    s = rand(-200, 200, 20)     # Start, End, List Length
+    s = rand(-200, 200, 20)
     print(s)
     map = FindMax(4)
     map.start(s)
