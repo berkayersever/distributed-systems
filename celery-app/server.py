a14bb7f4be19a5221d53a9501b21f1a3602ca2ab
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-04-01 23:48:20 +0300

    Implemented registration method

--- celery-app/server.py
+++ celery-app/server.py
@@ -1,3 +1,5 @@
+import json
+import time
 from flask import Flask
 from celery import Celery
 
@@ -25,3 +27,23 @@ def make_celery(app):
 
     c.Task = ContextTask
     return c
+
+
+celery = make_celery(app)
+
+
+@celery.task(name='server.registration')
+def registration(email):
+    with open('database.json', 'r') as db:
+        items = json.loads(db.read())
+
+    emails = [item['email'] for item in items]
+    if email not in emails:
+        items.append({'email': email})
+        # perform other required operations, send email etc.
+        time.sleep(10)
+        with open('database.json', 'w') as db:
+            db.write(json.dumps(items))
+        return True
+
+    return False
